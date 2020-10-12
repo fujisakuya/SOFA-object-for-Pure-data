347474fad81a38ca7206a02473f44963fe192847
s1250017 <40935790+s1250017@users.noreply.github.com>
2020-10-13 01:03:12 +0900

    Add files via upload

--- conv~/conv~.c
+++ conv~/conv~.c
@@ -4,28 +4,27 @@
 #include "m_pd.h"
 
 static t_class *conv_tilde_class;
-//インレットが2つ
-//1つめが元となる信号
-//2つめがIRのデータ
+//two inlets
+//First inlet is original signal
+//Second inlet is IR signal
 
 
 typedef struct _conv_tilde{
     t_object x_obj;  //
     
-    t_sample x_conv_before; //fft前の元データ
-    t_sample x_ir_before; //fft前のIRデータ
-    t_sample x_conv_after;//fft後の元データ
-    t_sample x_ir_after; //fft後のIRデータ
-    t_float f;//シグナルインレットにfloat型のメッセージが入った時それをシグナルに変えるときに使います。
-    
+    t_sample x_conv_before; //original data before fft
+    t_sample x_ir_before; //ir data before fft
+    t_sample x_conv_after;//original data after fft
+    t_sample x_ir_after; //ir data after fft
+    t_float f;//It is used to convert a float type message into a signal when it enters the signal inlet.
     
     fftw_complex *in_signal_fftw,*out_signal_fftw, *in_ir_fftw, *out_ir_fftw, *in_final, *out_final;
     fftw_plan plan1, plan2, plan3;
     
-    
     t_inlet *x_in2;
     t_outlet *x_out;
     
+    int fftsize;
     
 }t_conv_tilde;
 
@@ -44,28 +43,16 @@ t_int *conv_tilde_perform(t_int *w)
     int          n =           (int)(w[5]);
     
     int i;
-    int fftsize = 44100;
-    //int l = 44100;
+   // int fftsize = 44100;
+    double realD,imagD,realS,imagS;
+    double mux = 1.0/n;
+
+
     
     
+
     
-    
-    x->in_signal_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * fftsize);
-    x->out_signal_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) *fftsize);
-    x->plan1 = fftw_plan_dft_1d(fftsize, x->in_signal_fftw, x->out_signal_fftw, FFTW_FORWARD, FFTW_ESTIMATE);
-    
-    x->in_ir_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * fftsize);
-    x->out_ir_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) *fftsize);
-    x->plan2 = fftw_plan_dft_1d(fftsize, x->in_ir_fftw, x->out_ir_fftw, FFTW_FORWARD, FFTW_ESTIMATE);
-    
-    
-    x->in_final = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * fftsize);
-    x->out_final= (fftw_complex *)fftw_malloc(sizeof(fftw_complex) *fftsize);
-    x->plan3 = fftw_plan_dft_1d(fftsize,x->out_final,x->in_final, FFTW_BACKWARD, FFTW_ESTIMATE);
-    
-    
-    
-    for(i=0; i<fftsize; i++){
+    for(i=0; i<n; i++){
         x->in_signal_fftw[i][0] = in1[i];
         x->in_signal_fftw[i][1] = 0.0;
         
@@ -74,24 +61,11 @@ t_int *conv_tilde_perform(t_int *w)
     }
     
     
-    /*
-     for(; i<l; i++){
-     x->in_signal_fftw[i][0] = 0.0;
-     x->in_signal_fftw[i][1] = 0.0;
-     x->in_ir_fftw[i][0] = 0.0;
-     x->in_ir_fftw[i][1] = 0.0;
-     }
-     */
-    
-    
     fftw_execute(x->plan1);
     fftw_execute(x->plan2);
     
     
-    
-    
-    
-    for(i=0; i<fftsize; i++){
+    for(i=0; i<n; i++){
         x->out_final[i][0] =  x->out_signal_fftw[i][0] * x->out_ir_fftw[i][0];
         x->out_final[i][1] =  x->out_signal_fftw[i][1] * x->out_ir_fftw[i][1];
     }
@@ -99,21 +73,17 @@ t_int *conv_tilde_perform(t_int *w)
     
     
     
-    
-    /*
-     double mux = 1.0/fftsize;
-     
-     for( i = 0; i < fftsize; i++ )
+     for( i = 0; i < n; i++ )
      {
-     double realD = x->in_signal_fftw[i][0]; //freq_dataがin_dataのoutで、実数部分を入れます。
-     double imagD = x->in_signal_fftw[i][1]; //freq_dataがin_dataのoutで、虚数部分を入れます。
-     double realS = x->in_ir_fftw[i][0]; //freq_sequenceがin_sequenceのoutで、実数部分を入れます。
-     double imagS = x->in_ir_fftw[i][1]; //freq_sequenceがin_sequenceのoutで、実数部分を入れます。
+     realD = x->out_signal_fftw[i][0];
+     imagD = x->out_signal_fftw[i][1];
+     realS = x->out_ir_fftw[i][0];
+     imagS = x->out_ir_fftw[i][1];
      x->out_final[i][0] = (realD * realS - imagD * imagS)*mux;
      x->out_final[i][1] = (realD * imagS + imagD * realS)*mux;
-     
      }
-     */
+    
+    
     
     
     fftw_execute(x->plan3);
@@ -132,7 +102,23 @@ void conv_tilde_dsp(t_conv_tilde *x, t_signal **sp)
 {
     dsp_add(conv_tilde_perform, 5, x,
             sp[0]->s_vec, sp[1]->s_vec, sp[2]->s_vec, sp[0]->s_n);
+    
+    
+    x->in_signal_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * sp[0]->s_n);
+    x->out_signal_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) *sp[0]->s_n);
+    x->plan1 = fftw_plan_dft_1d(sp[0]->s_n, x->in_signal_fftw, x->out_signal_fftw, FFTW_FORWARD, FFTW_ESTIMATE);
+    
+    x->in_ir_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * sp[0]->s_n);
+    x->out_ir_fftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * sp[0]->s_n);
+    x->plan2 = fftw_plan_dft_1d(sp[0]->s_n, x->in_ir_fftw, x->out_ir_fftw, FFTW_FORWARD, FFTW_ESTIMATE);
+    
+    
+    x->in_final = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * sp[0]->s_n);
+    x->out_final= (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * sp[0]->s_n);
+    x->plan3 = fftw_plan_dft_1d(sp[0]->s_n,x->out_final,x->in_final, FFTW_BACKWARD, FFTW_ESTIMATE);
+    
 }
+
 
 
 
@@ -159,20 +145,11 @@ void conv_tilde_free(t_conv_tilde *x)
 void *conv_tilde_new(void)
 {
     t_conv_tilde *x = (t_conv_tilde *)pd_new(conv_tilde_class);
-    //オブジェクトが生成される度に呼び出される。
-    
+    // This is called every time an object is created.
     x->x_in2 = inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
-    //追加のシグナルインレット。引数はルックアップテーウルのシンボリックセレクター「シグナル」への参照。
+    //Additional signal inlet. The argument is a reference to the lookup tail symbolic selector "signal".
     x->x_out = outlet_new(&x->x_obj, &s_signal);
-    //アウトレットも同じように作られる。
-    
-    
-    
-    
-    
-    
-    
-    
+    //Additional signal outlet. This is made in same way.
     return (void *)x;
 }
 
@@ -194,14 +171,14 @@ void conv_tilde_setup(void){
     CLASS_MAINSIGNALIN(conv_tilde_class,t_conv_tilde,f);
     
 }
-//名前をconvにしたよ。
-//インストラクタ。毎回初期化されることを意味する
-//デスストラクタ。追加のインレットを使用したため必要
-//静的メモリの確保を行います。
-//オブジェクトのグラフィック設定。デフォにします。
-//シンボリックオブジェクトの引数が6つ以上必要ならばA_GIMMEを使用。終わりの０
-//dspセレクターをつけておくことでシグナルクラスになりA_CANTを持つことで不正なdspは送れなくなる
-//CLASS_MAINSIGNALINにより最初からあるインレットがシグナルインレットとなります。
+//Name object "conv~"
+//Instructor。Means that it is initialized every time
+//Destructor。 we need destructor because we created an additional inlet.
+//Allocate static memory.
+//Graphic settings for objects. Make it the default.
+//Use A_GIMME if you need more than 6 arguments for the symbolic object. 0 means End.
+//By attaching a dsp selector, it becomes a signal class, and by having A_CANT, invalid dsp cannot be sent.
+//In CLASS_MAINSIGNALIN, the inlet that exists from the beginning becomes the signal inlet.
 
 
 
