24c23c460002eb131e1902078ef7253295006b9f
Sakuya Fujisawa <40935790+s1250017@users.noreply.github.com>
2021-01-07 02:02:20 +0900

    Add files via upload

--- mysofa~/mysofa~.c
+++ mysofa~/mysofa~.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "fftw3.h"
 #include "m_pd.h"
 #include "mysofa.h"
 
@@ -12,12 +11,10 @@ static t_class *mysofa_tilde_class;
 
 #define MAX_BLOCKSIZE 8192
 
-typedef struct _mysofa_tilde{
+typedef struct _mysofa_tilde {
     t_object x_obj;  //
     t_float rightIR[MAX_BLOCKSIZE];
     t_float leftIR[MAX_BLOCKSIZE];
-    t_int filter_length;
-    t_int err;
     t_float f;
     t_float azimuth;
     t_float elevation;
@@ -26,17 +23,26 @@ typedef struct _mysofa_tilde{
     t_float x,y,z;
     t_float leftDelay;
     t_float rightDelay;
-
-
+    t_symbol *filenameArg;
+    t_float filter_length;
+    t_float err;
 
 
     t_inlet *x_in2;
     t_inlet *x_in3;
     t_inlet *x_in4;
+    t_inlet *x_in5;
     t_outlet *x_out1;
     t_outlet *x_out2;
-    struct MYSOFA_EASY *hrtf;
-}t_mysofa_tilde;
+
+    t_int M;    /* number of filters stored in sofa file */
+    t_int N;    /* numer of sample points per stored filter */
+    t_int R;    /* number of receivers */
+
+    struct MYSOFA_EASY *sofa;
+    char filename[1000];
+
+} t_mysofa_tilde;
 
 
 // MAX_BLOCKSIZE 8192
@@ -44,53 +50,111 @@ typedef struct _mysofa_tilde{
 //scaledBlocksize = blocksize * blockScale;
 //blocksizeDelta = MAX_BLOCKSIZE -1 - scaledBlocksize
 
+// t_symbol *filenameArg
 
-t_int *mysofa_tilde_perform(t_int *w)
-{
 
+/*
+// This function is copied from https://github.com/sofacoustics/SOFAlizer-for-pd/blob/master/SOFAlizer~/SOFAlizer~.c
+static void mysofa_tilde_open (t_mysofa_tilde *x, t_symbol *filenameArg) {
+    strcpy(x->filename, filenameArg->s_name);//->s_name);
+    if (x->filename[0] == '\0') {
+        error("No SOFA file specified");
+        return;
+    } else {
+        post("SOFA file %s will be opened..", x->filename);
+    }
+    int filter_length, err;
+    x->sofa = mysofa_open(x->filename, 48000, &filter_length, &err); // change this
+    if(x->sofa == NULL) {
+        error("SOFA file %s couldn't be opened.", x->filename);
+        return;
+    }
+    x->M = x->sofa->hrtf->M;    // number of filters stored in sofa file
+    x->N = x->sofa->hrtf->N;    // numer of sample points per stored filter
+    x->R = x->sofa->hrtf->R;    //number of receivers
+    
+    post("Metadata: %s %u HRTFs, %u samples @ %u Hz. %s, %s, %s.\n",
+         x->filename,(unsigned int)x->M, (unsigned int)x->N,
+         (unsigned int)(x->sofa->hrtf->DataSamplingRate.values[0]),
+         mysofa_getAttribute(x->sofa->hrtf->attributes, "DatabaseName"),
+         mysofa_getAttribute(x->sofa->hrtf->attributes, "Title"),
+         mysofa_getAttribute(x->sofa->hrtf->attributes, "ListenerShortName"));
+    
+    // Calculate distance of first measurement
+    int i;
+    for (i = 0; i < 3; i++) {    // read in first three coordinates
+        x->values[i] = x->sofa->hrtf->SourcePosition.values[i];
+    }
+    x->distance = sqrtf(powf(x->values[0], 2.f) + powf(x->values[1], 2.f) + powf(x->values[2], 2.f)); // calculate distance
+    post("Distance is %f m. \n", x->distance);
+    
+    // Check for IR delays
+    float delay = 0;
+    int warn = 0;
+    for (i = 0; i < x->R; i++) {
+        delay = x->sofa->hrtf->DataDelay.values[i];
+        if (delay != 0.0) {
+            warn = 1;
+        }
+    }
+    if (warn == 1) {
+        error(" Warning: This SOFA file will be incorrectly processed besause of non zero IR delays!"); // To change so we can use th delays too
+    }
+}
+
+*/
+
+t_int *mysofa_tilde_perform(t_int *w) {
     t_mysofa_tilde *x = (t_mysofa_tilde *)(w[1]);
     t_sample  *in =    (t_sample *)(w[2]);
     t_sample  *out1 =    (t_sample *)(w[3]);
     t_sample  *out2 =    (t_sample *)(w[4]);
     int       n =           (int)(w[5]);
-
     
     int i;
     x->leftDelay = 0;
     x->rightDelay = 0;
+    
+ 
 
-
-
-
-    x->values[0] = x->azimuth;
-    x->values[1] = x->elevation;
-    x->values[2] = x->distance;
+    x->values[0] = x->azimuth;//x->azimuth;
+    x->values[1] = x->elevation;//x->elevation;
+    x->values[2] = x->distance;//x->distance;
 
     mysofa_s2c(x->values);
+    
+    //post("%f, ",x->values[0]);
+    //post("%f, ",x->values[1]);
+    //post("%f\n",x->values[2]);
 
     x->x = x->values[0];
     x->y = x->values[1];
     x->z = x->values[2];
+    
+    
 
-s
-
-    mysofa_getfilter_float(x->hrtf, x->x, x->y, x->z,x->leftIR,x->rightIR,&x->leftDelay,&x->rightDelay);
-
-   for(i=0;i<n;i++){
-        out1[i] = x->leftIR[i];
-        out2[i] = x->rightIR[i];
+    mysofa_getfilter_float(x->sofa, x->x, x->y, x->z,x->leftIR,x->rightIR,&x->leftDelay,&x->rightDelay);
+    
+    /*
+       for(i=0;i<n;i++){
+            out1[i] = x->leftIR[i];
+            out2[i] = x->rightIR[i];
+        }
+    
+    
+    
+    for(i=0;i<n;i++){
+    post("%f, ",x->leftIR[i]);
     }
+    
+ */
 
-
+     
     return (w+6);
 
 }
 
-
-
-void mysofa_tilde_dsp(t_mysofa_tilde *x, t_signal **sp)
-{
-    
+void mysofa_tilde_dsp(t_mysofa_tilde *x, t_signal **sp) {
     dsp_add(mysofa_tilde_perform,
             5,
             x,
@@ -98,43 +162,58 @@ void mysofa_tilde_dsp(t_mysofa_tilde *x, t_signal **sp)
             sp[1]->s_vec, //out_signal_r
             sp[2]->s_vec, //out_signal_l
             sp[0]->s_n);
+
+    int filter_length, err,i;
     
+        //x->sofa = mysofa_open("/Users/fujisawasakuwataru/Downloads/Pd/mysofa/RIEC_hrtf_all/RIEC_hrir_subject_001.sofa", 48000, &filter_length, &err);
     
-    x->hrtf = (struct MYSOFA_EASY*)malloc(sizeof(struct MYSOFA_EASY));
-    x->hrtf = mysofa_open("RIEC_hrir_subject_001.sofa", 48000, &x->filter_length, &x->err);
+    x->sofa = mysofa_open(x->filenameArg->s_name, 48000, &filter_length,&err);
+        //mysofa_tilde_open(x, x->filenameArg);
+    x->filter_length = filter_length;
+    
+    for(i=0; i<x->filter_length; i++){
+        x->rightIR[i] = 0.0;
+        x->leftIR[i] = 0.0;
+    }
     
 
+    if(!x->sofa) {
+        post("Error opening the SOFA file");
+    }
+    post("%d, ",err);
+    
 }
 
 
 
 
-void mysofa_tilde_free(t_mysofa_tilde *x)
-{
+void mysofa_tilde_free(t_mysofa_tilde *x) {
     inlet_free(x->x_in2);
     inlet_free(x->x_in3);
     inlet_free(x->x_in4);
+    inlet_free(x->x_in5);
     outlet_free(x->x_out1);
     outlet_free(x->x_out2);
-    mysofa_close(x->hrtf);
-
+    mysofa_close(x->sofa);
 }
 
 
 
 
-void *mysofa_tilde_new(t_floatarg f)
-{
+void *mysofa_tilde_new(t_symbol *filenameArg) {
     t_mysofa_tilde *x = (t_mysofa_tilde *)pd_new(mysofa_tilde_class);
-    // This is called every time an object is created.
-    x->x_in2 = floatinlet_new(&x->x_obj, &x->azimuth);
-    x->x_in3 = floatinlet_new(&x->x_obj, &x->elevation);
-    x->x_in4 = floatinlet_new(&x->x_obj, &x->distance);
+   //  This is called every time an object is created.
+    x->x_in2 = symbolinlet_new(&x->x_obj, &x->filenameArg);
+    x->x_in3 = floatinlet_new(&x->x_obj, &x->azimuth);
+    x->x_in4 = floatinlet_new(&x->x_obj, &x->elevation);
+    x->x_in5 = floatinlet_new(&x->x_obj, &x->distance);
+
 
 
     x->x_out1 = outlet_new(&x->x_obj, &s_signal);
     x->x_out2 = outlet_new(&x->x_obj, &s_signal);
-
+    
+    
     //Additional signal outlet. This is made in same way.
     return (void *)x;
 }
@@ -143,7 +222,7 @@ void *mysofa_tilde_new(t_floatarg f)
 
 
 
-void mysofa_tilde_setup(void){
+void mysofa_tilde_setup(void) {
 
     mysofa_tilde_class=class_new(gensym("mysofa~"),
                                  (t_newmethod)mysofa_tilde_new,
@@ -157,9 +236,9 @@ void mysofa_tilde_setup(void){
                     gensym("dsp"),A_CANT,0);
     CLASS_MAINSIGNALIN(mysofa_tilde_class, t_mysofa_tilde, f);
 
-    post("singen~ (c) 2020 hoge hoge");
-
+   // class_addmethod(mysofa_tilde_class, (t_method)mysofa_tilde_open, gensym("open"), A_DEFSYMBOL, 0); /* method to open new sofa file */
 }
+
 //Name object "mysofa~"
 //Instructor。Means that it is initialized every time
 //Destructor。 we need destructor because we created an additional inlet.
---
+++ mysofa~/mysofa~copi.c
@@ -0,0 +1,452 @@
+#include <math.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "m_pd.h"
+#include <mysofa.h>
+#include <fftw3.h>
+
+static t_class *mysofa_tilde_class;
+
+#define MAX_BLOCKSIZE 8192
+#define MAX_N_POINTS 3000
+
+
+typedef struct _mysofa_tilde {
+    t_object x_obj;  //
+    t_float rightIR[MAX_BLOCKSIZE];
+    t_float leftIR[MAX_BLOCKSIZE];
+    t_float f;
+    t_float azimuth;
+    t_float elevation;
+    t_float distance;
+    t_float values[3];
+    t_float x,y,z;
+    t_float leftDelay;
+    t_float rightDelay;
+    t_float fftsizeArg;
+    t_float fftsize;
+    t_float convsize;
+    t_float flag;
+    t_symbol *filenameArg;
+    t_float filter_length;
+    t_int err;
+    
+    
+    t_sample x_conv_before; //original data before fft
+    t_sample x_ir_before; //ir data before fft
+    t_sample x_conv_after;//original data after fft
+    t_sample x_ir_after; //ir data after fft
+    t_sample buffer_right[MAX_BLOCKSIZE];
+    t_sample buffer_left[MAX_BLOCKSIZE];
+    t_sample previousImpulse[MAX_N_POINTS];
+    t_float crossCoef[MAX_BLOCKSIZE];
+    t_int bufferPin;
+    t_int nPts;//It is used to convert a float type message into a signal when it enters the signal inlet.
+
+
+    t_inlet *x_in2;
+    t_inlet *x_in3;
+    t_inlet *x_in4;
+    t_inlet *x_in5;
+    t_outlet *x_out1;
+    t_outlet *x_out2;
+
+    t_int M;    /* number of filters stored in sofa file */
+    t_int N;    /* numer of sample points per stored filter */
+    t_int R;    /* number of receivers */
+
+    struct MYSOFA_EASY *sofa;
+    char filename[1000];
+    fftwf_complex *in_signal_fftwf,*out_signal_fftwf;
+    fftwf_complex *in_left_ir_fftwf, *out_left_ir_fftwf,*in_right_ir_fftwf,*out_right_ir_fftwf;
+    fftwf_complex *in_left_final, *out_left_final, *in_right_final, *out_right_final;
+
+    fftwf_plan plan1, plan2, plan3, plan4,plan5;
+
+
+} t_mysofa_tilde;
+
+
+// MAX_BLOCKSIZE 8192
+//blockScale = MAX_BLOCKSIZE / blocksize;
+//scaledBlocksize = blocksize * blockScale;
+//blocksizeDelta = MAX_BLOCKSIZE -1 - scaledBlocksize
+
+// t_symbol *filenameArg
+
+
+/*
+// This function is copied from https://github.com/sofacoustics/SOFAlizer-for-pd/blob/master/SOFAlizer~/SOFAlizer~.c
+static void mysofa_tilde_open (t_mysofa_tilde *x, t_symbol *filenameArg) {
+    strcpy(x->filename, filenameArg->s_name);//->s_name);
+    if (x->filename[0] == '\0') {
+        error("No SOFA file specified");
+        return;
+    } else {
+        post("SOFA file %s will be opened..", x->filename);
+    }
+    int filter_length, err;
+    x->sofa = mysofa_open(x->filename, 48000, &filter_length, &err); // change this
+    if(x->sofa == NULL) {
+        error("SOFA file %s couldn't be opened.", x->filename);
+        return;
+    }
+    x->M = x->sofa->hrtf->M;    // number of filters stored in sofa file
+    x->N = x->sofa->hrtf->N;    // numer of sample points per stored filter
+    x->R = x->sofa->hrtf->R;    //number of receivers
+    
+    post("Metadata: %s %u HRTFs, %u samples @ %u Hz. %s, %s, %s.\n",
+         x->filename,(unsigned int)x->M, (unsigned int)x->N,
+         (unsigned int)(x->sofa->hrtf->DataSamplingRate.values[0]),
+         mysofa_getAttribute(x->sofa->hrtf->attributes, "DatabaseName"),
+         mysofa_getAttribute(x->sofa->hrtf->attributes, "Title"),
+         mysofa_getAttribute(x->sofa->hrtf->attributes, "ListenerShortName"));
+    
+    // Calculate distance of first measurement
+    int i;
+    for (i = 0; i < 3; i++) {    // read in first three coordinates
+        x->values[i] = x->sofa->hrtf->SourcePosition.values[i];
+    }
+    x->distance = sqrtf(powf(x->values[0], 2.f) + powf(x->values[1], 2.f) + powf(x->values[2], 2.f)); // calculate distance
+    post("Distance is %f m. \n", x->distance);
+    
+    // Check for IR delays
+    float delay = 0;
+    int warn = 0;
+    for (i = 0; i < x->R; i++) {
+        delay = x->sofa->hrtf->DataDelay.values[i];
+        if (delay != 0.0) {
+            warn = 1;
+        }
+    }
+    if (warn == 1) {
+        error(" Warning: This SOFA file will be incorrectly processed besause of non zero IR delays!"); // To change so we can use th delays too
+    }
+}
+
+*/
+
+
+
+t_int *mysofa_tilde_perform(t_int *w) {
+    t_mysofa_tilde *x = (t_mysofa_tilde *)(w[1]);
+    t_sample  *in =    (t_sample *)(w[2]);
+    t_sample  *out1 =    (t_sample *)(w[3]);
+    t_sample  *out2 =    (t_sample *)(w[4]);
+    int       n =           (int)(w[5]);
+    
+    float blocksize = n;
+    int blockScale  = MAX_BLOCKSIZE / blocksize;
+    unsigned scaledBlocksize;
+    int i = 0;
+    float realD,imagD,realS,imagS;
+    float mux = 1.0/blocksize;
+    
+    
+    x->leftDelay = 0;
+    x->rightDelay = 0;
+    
+ 
+
+    x->values[0] = x->azimuth;//x->azimuth;
+    x->values[1] = x->elevation;//x->elevation;
+    x->values[2] = x->distance;//x->distance;
+
+    mysofa_s2c(x->values);
+    
+    post("%f, ",x->values[0]);
+    post("%f, ",x->values[1]);
+    post("%f\n",x->values[2]);
+
+    x->x = x->values[0];
+    x->y = x->values[1];
+    x->z = x->values[2];
+
+   // mysofa_getfilter_float(x->sofa,x->x,x->y,x->z,x->leftIR,x->rightIR,&x->leftDelay,&x->rightDelay);
+    
+    
+    /*
+     
+    //signal
+    for( i = 0; i<n ; i++){
+        x->in_signal_fftwf[i][0] = in[i];
+        x->in_signal_fftwf[i][1] = 0.0;
+    }
+    
+    
+    for( i = n; i<(x->fftsize-n) ; i++){
+        x->in_signal_fftwf[i][0] = 0.0;
+        x->in_signal_fftwf[i][1] = 0.0;
+    }
+     
+    
+     */
+    
+    
+    /*
+    
+    //left IR
+    for(i =0;i<x->filter_length;i++){
+        scaledBlocksize = (x->filter_length-i)*blockScale;
+        x->in_left_ir_fftwf[i][0] = x->leftIR[i] * x->crossCoef[scaledBlocksize] + x->previousImpulse[i] * x->crossCoef[MAX_BLOCKSIZE - scaledBlocksize];
+        x->in_left_ir_fftwf[i][1] = 0.0;
+        x->previousImpulse[i] = x->in_left_ir_fftwf[i][0];
+    }
+    
+    for(i =x->filter_length;i<(x->fftsize-x->filter_length);i++){
+        x->in_left_ir_fftwf[i][0] = 0.0;
+        x->in_left_ir_fftwf[i][1] = 0.0;
+    }
+    
+    
+    
+    
+
+    //right IR
+    for(i =0;i<x->filter_length;i++){
+        scaledBlocksize = (x->filter_length-i)*blockScale;
+        x->in_right_ir_fftwf[i][0] = x->leftIR[i] * x->crossCoef[scaledBlocksize] + x->previousImpulse[i] * x->crossCoef[MAX_BLOCKSIZE - scaledBlocksize];
+        x->in_right_ir_fftwf[i][1] = 0.0;
+        x->previousImpulse[i] = x->in_right_ir_fftwf[i][0];
+    }
+    
+    for(i =x->filter_length;i<(x->fftsize-x->filter_length);i++){
+        x->in_left_ir_fftwf[i][0] = 0.0;
+        x->in_left_ir_fftwf[i][1] = 0.0;
+    }
+     
+     */
+    
+    
+    /*
+     
+    
+    fftwf_execute(x->plan1);
+    fftwf_execute(x->plan2);
+    fftwf_execute(x->plan3);
+     
+     */
+    
+    
+    
+    /*
+    for( i = 0; i < x->convsize; i++ ){
+        realD = x->out_signal_fftwf[i][0];
+        imagD = x->out_signal_fftwf[i][1];
+        realS = x->out_left_ir_fftwf[i][0];
+        imagS = x->out_left_ir_fftwf[i][1];
+        x->out_left_final[i][0] = (realD * realS - imagD * imagS)*mux;
+        x->out_left_final[i][1] = (realD * imagS + imagD * realS)*mux;
+    }
+    
+    
+     
+    for( i = 0; i < x->convsize; i++ ){
+        realD = x->out_signal_fftwf[i][0];
+        imagD = x->out_signal_fftwf[i][1];
+        realS = x->out_right_ir_fftwf[i][0];
+        imagS = x->out_right_ir_fftwf[i][1];
+        x->out_right_final[i][0] = (realD * realS - imagD * imagS)*mux;
+        x->out_right_final[i][1] = (realD * imagS + imagD * realS)*mux;
+    }
+     */
+    
+    
+    /*
+        fftwf_execute(x->plan4);
+        fftwf_execute(x->plan5);
+     */
+    
+    
+    
+    /*
+    for(i=x->convsize;i<x->fftsize-x->convsize;i++){
+        x->in_right_final[i][0] = 0.0;
+        x->in_left_final[i][0] = 0.0;
+    }
+     */
+    
+
+    /*
+    if(x->flag == 0){
+        
+        for(i=0;i<n;i++){
+            out1[i] = x->in_right_final[i][0];
+            out2[i] = x->in_left_final[i][0];
+        }
+    
+        for(i=0; i<x->filter_length; i++){
+            x->buffer_right[i] = x->in_right_final[i+n][0];
+            x->buffer_left[i] = x->in_left_final[i+n][0];
+        
+            x->flag = 1;
+        }
+    }
+    
+    
+    
+   else if(x->flag == 1){
+        for(i =x->filter_length;i<x->convsize;i++){
+            x->buffer_right[i] = 0.0;
+            x->buffer_left[i] = 0.0;
+        }
+        
+        for(i = 0;i<x->convsize;i++){
+            x->buffer_right[i] = x->buffer_right[i] + x->in_right_final[i][0];
+            x->buffer_left[i] = x->buffer_left[i] + x->in_left_final[i][0];
+        }
+       
+       for(i=0;i<n;i++){
+           out1[i] = x->buffer_right[i];
+           out2[i] = x->buffer_left[i];
+       }
+        
+    }
+     */
+    
+    
+    /*
+    for(i=0;i<n;i++){
+    post("%f, ",x->leftIR[i]);
+    }
+    */
+    return (w+6);
+
+}
+
+
+
+
+
+void mysofa_tilde_dsp(t_mysofa_tilde *x, t_signal **sp) {
+    dsp_add(mysofa_tilde_perform,
+            5,
+            x,
+            sp[0]->s_vec, //in_signal
+            sp[1]->s_vec, //out_signal_r
+            sp[2]->s_vec, //out_signal_l
+            sp[0]->s_n);
+
+    int filter_length, err;
+    int i=0;
+    int size[8] = {128, 256, 512, 1024, 2048, 4096, 8192};
+    
+   // x->sofa = mysofa_open("/Users/fujisawasakuwataru/Downloads/Pd/mysofa/RIEC_hrtf_all/RIEC_hrir_subject_001.sofa", 48000, &filter_length, &err);
+    
+      x->sofa = mysofa_open(x->filenameArg->s_name, 48000, &filter_length, &err);
+     //mysofa_tilde_open(x, x->filenameArg);
+      x->filter_length = filter_length;
+      x->convsize = x->filter_length + sp[0]->s_n;
+    
+    while(1){
+        if(i==8){
+            post("blocksize is too large");
+            break;
+        }
+        if(x->fftsize<=size[i]){
+            x->fftsize = size[i];
+            break;
+        }
+        i++;
+    }
+    
+    
+    if(!x->sofa) {
+        post("Error opening the SOFA file");
+    }
+    
+    x->in_signal_fftwf = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->out_signal_fftwf = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->plan1 = fftwf_plan_dft_1d(x->fftsize, x->in_signal_fftwf, x->out_signal_fftwf, FFTW_FORWARD, FFTW_ESTIMATE);
+    
+    x->in_left_ir_fftwf = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->out_left_ir_fftwf = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->plan2 = fftwf_plan_dft_1d(x->fftsize, x->in_left_ir_fftwf, x->out_left_ir_fftwf, FFTW_FORWARD, FFTW_ESTIMATE);
+    
+    x->in_right_ir_fftwf = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->out_right_ir_fftwf = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->plan3 = fftwf_plan_dft_1d(x->fftsize, x->in_left_ir_fftwf, x->out_left_ir_fftwf, FFTW_FORWARD, FFTW_ESTIMATE);
+
+    
+    x->in_left_final = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->out_left_final= (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->plan4 = fftwf_plan_dft_1d(x->fftsize,x->out_left_final,x->in_left_final, FFTW_BACKWARD, FFTW_ESTIMATE);
+    
+    x->in_right_final = (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->out_right_final= (fftwf_complex *)fftwf_malloc(sizeof(fftwf_complex) * x->fftsize);
+    x->plan5 = fftwf_plan_dft_1d(x->fftsize,x->out_right_final,x->in_right_final, FFTW_BACKWARD, FFTW_ESTIMATE);
+    
+    
+    for(i = 0; i<x->fftsize; i++){
+        x->previousImpulse[i] = 0;
+    }
+    
+}
+
+
+
+
+
+
+void mysofa_tilde_free(t_mysofa_tilde *x) {
+    inlet_free(x->x_in2);
+    inlet_free(x->x_in3);
+    inlet_free(x->x_in4);
+    inlet_free(x->x_in5);
+    outlet_free(x->x_out1);
+    outlet_free(x->x_out2);
+    mysofa_close(x->sofa);
+}
+
+
+
+
+void *mysofa_tilde_new(t_symbol *filenameArg) {
+    t_mysofa_tilde *x = (t_mysofa_tilde *)pd_new(mysofa_tilde_class);
+   //  This is called every time an object is created.
+    x->x_in2 = symbolinlet_new(&x->x_obj, &x->filenameArg);
+    x->x_in3 = floatinlet_new(&x->x_obj, &x->azimuth);
+    x->x_in4 = floatinlet_new(&x->x_obj, &x->elevation);
+    x->x_in5 = floatinlet_new(&x->x_obj, &x->distance);
+
+
+    x->x_out1 = outlet_new(&x->x_obj, &s_signal);
+    x->x_out2 = outlet_new(&x->x_obj, &s_signal);
+    
+    
+    //Additional signal outlet. This is made in same way.
+    return (void *)x;
+}
+
+
+
+
+
+void mysofa_tilde_setup(void) {
+
+    mysofa_tilde_class=class_new(gensym("mysofa~"),
+                                 (t_newmethod)mysofa_tilde_new,
+                                 (t_method)mysofa_tilde_free,
+                                 sizeof(t_mysofa_tilde),
+                                 CLASS_DEFAULT,
+                                 A_DEFFLOAT,0);
+
+    class_addmethod(mysofa_tilde_class,
+                    (t_method)mysofa_tilde_dsp,
+                    gensym("dsp"),A_CANT,0);
+    CLASS_MAINSIGNALIN(mysofa_tilde_class, t_mysofa_tilde, f);
+
+   // class_addmethod(mysofa_tilde_class, (t_method)mysofa_tilde_open, gensym("open"), A_DEFSYMBOL, 0); /* method to open new sofa file */
+}
+
+//Name object "mysofa~"
+//Instructor。Means that it is initialized every time
+//Destructor。 we need destructor because we created an additional inlet.
+//Allocate static memory.
+//Graphic settings for objects. Make it the default.
+//Use A_GIMME if you need more than 6 arguments for the symbolic object. 0 means End.
+//By attaching a dsp selector, it becomes a signal class, and by having A_CANT, invalid dsp cannot be sent.
+//In CLASS_MAINSIGNALIN, the inlet that exists from the beginning becomes the signal inlet.
